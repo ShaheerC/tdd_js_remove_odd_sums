d1bdcc9829c0d5b81b99ea27e851ee5a1f12010c
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-09-01 22:50:20 -0400

    finished reinforcement

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function removeOddSum() {
+function removeOddSum(array) {
+    array.forEach((element, index) => {
+
+        if ((element.a + element.b) % 2 !== 0) {
+            array.splice(index, 1);
+        }
+    })
+    return array;
 }
 
 module.exports = removeOddSum;
